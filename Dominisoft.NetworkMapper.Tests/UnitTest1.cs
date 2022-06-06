ce4787a62379f3d8bd510ff4de8d25d169572b86
Dominic Hemken <dphemken@gmail.com>
2022-06-06 18:50:22 -0500

    Add project files.

---
+++ Dominisoft.NetworkMapper.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Dominisoft.NetworkMapper.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
